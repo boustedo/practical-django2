929c526e79eb6b8467ca81e713e5a54eb0df22f0
Stéphane Bouchard <boustedo@gmail.com>
2019-08-13 10:16:39 -0400

    End of Managing Data Through Django admin

--- app/main/models.py
+++ app/main/models.py
@@ -9,6 +9,9 @@ class Product(models.Model):
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE
@@ -18,10 +21,12 @@ class ProductImage(models.Model):
         upload_to="product-thumbnails", null=True
     )
 
-
 class ProductTag(models.Model):
     products = models.ManyToManyField(Product, blank=True)
     name = models.CharField(max_length=32)
     slug = models.SlugField(max_length=48)
     description = models.TextField(blank=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
