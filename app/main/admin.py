812ac0377eb6336dd5239bf57547ebf959d2be1d
Stéphane Bouchard <boustedo@gmail.com>
2019-08-13 15:26:45 -0400

    Before ch3 - Adding a Product List Page

--- app/main/admin.py
+++ app/main/admin.py
@@ -8,7 +8,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('active', 'in_stock', 'date_updated')
     list_editable = ('in_stock', )
     search_fields = ('name',)
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {"slug": ("name",)}   
+    autocomplete_fields = ('tags',)
 
 admin.site.register(models.Product, ProductAdmin)
 
@@ -17,7 +18,6 @@ class ProductTagAdmin(admin.ModelAdmin):
     list_filter = ('active',)
     search_fields = ('name',)
     prepopulated_fields = {"slug": ("name",)}
-    autocomplete_fields = ('products',)
 
 admin.site.register(models.ProductTag, ProductTagAdmin)
 
--- app/main/models.py
+++ app/main/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    objects = ProductTagManager()
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.slug,)
+
 class Product(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
@@ -8,10 +25,11 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
 
     def __str__(self):
         return self.name
-
+    
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE
@@ -20,13 +38,3 @@ class ProductImage(models.Model):
     thumbnail = models.ImageField(
         upload_to="product-thumbnails", null=True
     )
-
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
